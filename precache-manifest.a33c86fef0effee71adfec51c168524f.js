5bb0504c5ae167c3893fa1d5ca51164ff2a5c5b4
i.sushko <2304834777>
2019-01-17 18:41:45 +0200

    Updates

---
+++ precache-manifest.a33c86fef0effee71adfec51c168524f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "6eda3251968340712a7e",
+    "url": "/contactmanager/static/js/main.6eda3251.chunk.js"
+  },
+  {
+    "revision": "2bc4d73b3ad7c8e55e68",
+    "url": "/contactmanager/static/js/1.2bc4d73b.chunk.js"
+  },
+  {
+    "revision": "6eda3251968340712a7e",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2bc4d73b3ad7c8e55e68",
+    "url": "/contactmanager/static/css/1.77011820.chunk.css"
+  },
+  {
+    "revision": "dadfd4b652b7617cbed6b489ab6a56b2",
+    "url": "/contactmanager/index.html"
+  }
+];
